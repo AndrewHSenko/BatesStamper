40dd464990413cbaa7a90ba14d30d24da82d5d2c
Andrew Senkowski <andrewsenkowski@g.ucla.edu>
2022-04-28 00:08:26 -0700

    Created the skeleton and script to apply the stamps once the stamps has been created

---
+++ produce_stamps.py
@@ -0,0 +1,14 @@
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+from reportlab.lib import colors
+
+def make_stamp_text(stamp):
+    # Create PDF of stamp
+    pg_num = f'{i:06d}'
+    return
+        
+
+# FILE MUST BE {ex #} stamps
+def main(file_name, stamp):
+    return make_stamp_text(stamp)
